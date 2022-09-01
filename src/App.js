91b7c324f2dbc6594bc88d1fe207cd4d91cc530b
jcable <julian.cable@bbc.co.uk>
2022-09-01 22:43:57 +0100

    tests

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function App() {
   const action = () => axios.get('https://ypdjc6zbc5cnvth24lk3mm45sm0qtgps.lambda-url.eu-west-1.on.aws/britbox_us_barker_one/eu-west-1');
 
   return (
-    <Timer millis={5000} action={action} /> 
+    <div>
+    <Timer millis={5000} action={action} />
+      <div>a demo</div>
+    </div>
   );
  }

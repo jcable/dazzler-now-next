5d369076579450541b78cc15a878235f6da5ac12
jcable <julian.cable@bbc.co.uk>
2022-09-03 11:01:19 +0100

    fix wip

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ export default function App() {
   const b=0;
   return (
     <Box sx={{
-      width: '100%', height: '95vh',
+      width: '100%', height: '90vh',
       backgroundColor: alpha('#000', 0),
       display: 'grid', gridTemplateRows: '1fr 1fr 1fr'
      }}>

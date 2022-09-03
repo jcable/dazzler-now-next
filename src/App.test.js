4a31bc1a08927123fc429e11ea0943edee38187d
jcable <julian.cable@bbc.co.uk>
2022-09-03 17:30:43 +0100

    fix: test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/a demo/i);
+  const linkElement = screen.getByAltText(/cbeebies/i);
   expect(linkElement).toBeInTheDocument();
 });

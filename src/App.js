f8b6ef38c9b4688943071ee3d5c71c2dba987193
jcable <julian.cable@bbc.co.uk>
2022-09-03 08:29:52 +0100

    feat: work for any service using query string

--- src/App.js
+++ src/App.js
@@ -8,46 +8,68 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import logo from './CBeebies.png'
+
+const nn = 'https://ypdjc6zbc5cnvth24lk3mm45sm0qtgps.lambda-url.eu-west-1.on.aws';
+
+function chooseNext(next) {
+  if (next.length>0) {
+    return next[0]; // can be made more sophisticated
+  }
+  return undefined;
+}
 
 function NowNext() {
 
   const [text, setText] = useState('');
+  const [now, setNow] = useState();
+  const [next, setNext] = useState();
   const [vis, setVis] = useState(false);
 
-  const update = () => {
-    const d = new Date();
-    if ([0,5,10,15,20,25,30,35,40,45,50,55].includes(d.getMinutes())) {
+  useEffect(() => {
+    const q = new URLSearchParams(window.location.search);
+    let interval = null;
+    interval = setInterval(() => {
       (async () => {
-        const r = await axios.get('https://ypdjc6zbc5cnvth24lk3mm45sm0qtgps.lambda-url.eu-west-1.on.aws/britbox_us_barker_one/eu-west-1');
-        const next = r.data.next;
-        const now = r.data.now;
-        if (next.length>0) {
-          const start = Date.parse(next[0].start);
-          const minutesToNext = Math.round((start-d)/1000/60);
-          console.log('minutes to next start', minutesToNext);
-          if (minutesToNext > 10) {
-            setText(`now playing: ${now.title}`);
-          } else {
-            setText(`next up: ${next[0].title}`);
-          }
-        } else {
-          setText(`now playing: ${now.title}`);
-        }
+        const r = await axios.get(`${nn}/${q.get('sid')}/${q.get('region')}`);
+        setNext(chooseNext(r.data.next));
+        setNow(r.data.now);
       })();  
-    }
-    if ([0,1,2,3,4,5].includes(d.getSeconds())) {
-      setVis(true);
-    } else {
-      setVis(true);
-    }
-  };
+    }, 30000);
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
-    let interval = null;
-    interval = setInterval(update, 5000);
-    return () => clearInterval(interval);
+    console.log('setText');
+    let r;
+    if (next) {
+      const start = Date.parse(next.start);
+      const minutesToNext = Math.round((start-(new Date()))/1000/60);
+      console.log('minutes to next start', minutesToNext);
+      if (minutesToNext <= 10) {
+        r = `next up: ${next.title}`;
+      }
+    }
+    if (r) {
+      setText(r);
+    } else {
+      if (now) {
+        setText(`now playing: ${now.title}`);
+      } else {
+        setText('');
+      }
+    }
+  }, [next, now]);
+
+  useEffect(() => {
+    console.log('setVis');
+    if (text === '') {
+      setVis(false);
+    } else {
+      setVis([0,1,2,3,4,5].includes((new Date()).getSeconds()));
+    }
   }, [text]);
-  console.log(vis, text);
+
   if (vis) {
     return (
       <Fade in={true} timeout={2000}>
@@ -56,12 +78,13 @@ function NowNext() {
         </Box>
       </Fade>
     );    
+  } else {
+    return (
+      <Box sx={{margin: '5%', color: '#F0F8FF', backgroundColor: alpha("#000080", 0)}}>
+        <Typography>&nbsp;</Typography>
+      </Box>
+    );
   }
-  return (
-    <Box sx={{margin: '5%', color: '#F0F8FF', backgroundColor: alpha("#000080", 0)}}>
-      <Typography>&nbsp;</Typography>
-    </Box>
-  );
 }
 
 export default function App() {
@@ -73,10 +96,7 @@ export default function App() {
       display: 'grid', gridTemplateRows: '1fr 1fr 1fr'
      }}>
       <Box sx={{border: b, display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
-        <Box sx={{border: b}}><video
-        autoPlay={true} loop={true} width={150}>
-        <source src='https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4' type='video/mp4'/>
-        </video></Box>
+        <Box sx={{border: b}}><img src={logo} alt='CBeebies'/></Box>
         <Box sx={{border: b}}></Box>
         <Box sx={{border: b, display: 'block', marginLeft: 'auto'}}><img alt='bounce' src='https://upload.wikimedia.org/wikipedia/commons/1/14/Animated_PNG_example_bouncing_beach_ball.png'/></Box>
       </Box>

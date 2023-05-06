04cf988f93a11cf6d1292af16bb58a825d925307
Julian Cable <julian.cable@bbc.co.uk>
2023-05-06 06:43:25 +0100

    use param

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App tl={true}/>);
   const linkElement = screen.getByAltText(/cbeebies/i);
   expect(linkElement).toBeInTheDocument();
 });

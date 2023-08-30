0d92850b928073954cec22f04a2bf0f957555e32
jcable <julian.cable@bbc.co.uk>
2023-08-30 14:56:00 +0100

    fix: font size

--- src/App.js
+++ src/App.js
@@ -171,6 +171,7 @@ function Bottom({ params }) {
       onEntered={() => console.log('entered')}
        addEndListener={() => setSteady(false)}>
       <Box sx={{
+        fontSize: 40,
         width: 'auto', color: '#ffffff',
         background: 'linear-gradient(to right, rgba(15, 15, 15, .7), rgba(245, 73, 151, .7))',
         display: 'grid', gridTemplateColumns: '1fr 4fr 1fr'
